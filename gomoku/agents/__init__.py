038b1239f712403c25046e896808db9cdae12bfa
Taukoor, Hemen Kevin <htaukoor@illumina.com>
2025-08-05 22:13:49 +0800

    Initial commit

--- gomoku/agents/__init__.py
+++ gomoku/agents/__init__.py
@@ -2,6 +2,7 @@ from .base import Agent
 from .simple_agent import SimpleGomokuAgent, SimpleAgent
 from .openai_llm_agent import LLMGomokuAgent
 from .hf_llm_agent import HfGomokuAgent
+from .my_llm_agent import MyLLMGomokuAgent
 
 __all__ = [
     # Base classes
